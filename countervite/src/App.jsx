2284130174d2269b31789b80068c0a941d014797
userAshwani <codeashwani@gmail.com>
2024-10-20 09:37:02 +0530

    update in counter project an interview question

--- countervite/src/App.jsx
+++ countervite/src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(5)
+  let [counter, setCounter] = useState(3)
 
   const addValue = () =>{
-    setCounter(counter + 1)
+
+    // know this all combine and behaving like bunch of package as they got they have assign same work to do
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    
+    // to overcome this problem
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
   const subValue = () =>{
     if(counter>0){
--- propstailwindcss/src/App.jsx
+++ propstailwindcss/src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from 'react'
 import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
 
       <h2 className='bg-green-700'>Props & Tailwind</h2>
       <Card username="ash1" para="1" />
